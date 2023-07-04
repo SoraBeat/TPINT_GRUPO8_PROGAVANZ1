4d7522a862ddc24f265d9a16f8f808c64496b3a3
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2023-07-04 20:34:50 -0300

    Redirigir al Inicio si ya estoy loguado

--- TPINT_GRUPO_8_PROG/src/controller/LoginController.java
+++ TPINT_GRUPO_8_PROG/src/controller/LoginController.java
@@ -2,6 +2,8 @@ package controller;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,8 +17,13 @@ public class LoginController {
 	@RequestMapping(value="/login.html", method = RequestMethod.GET)
 	public ModelAndView eventoRedireccionarLogin() {
 		ModelAndView MV = (ModelAndView) appContext.getBean("beanModelView");
-		MV.setViewName("Login");
-		return MV;
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth.getPrincipal() != "anonymousUser" ) {
+        	MV.setViewName("Inicio"); // Nombre de la vista que deseas redireccionar
+        } else {
+        	MV.setViewName("Login"); // Nombre de la vista de login
+        }
+        return MV;
 	}
 	
 	@RequestMapping(value="/home.html", method = RequestMethod.GET)
